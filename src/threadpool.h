de4c379687f3a7b9acff10954e118a41739a49be
skctvc15 <494263362@qq.com>
2015-04-19 19:35:40 +0800

    tools

--- src/threadpool.h
+++ src/threadpool.h
@@ -8,11 +8,11 @@
 #ifndef THREADPOOL_H
 #define THREADPOOL_H
 
-#include <list>
-#include <cstdio>
+#include<list>
+#include<cstdio>
 #include<stdexcept>
 #include<pthread.h>
-#include"lock.h"
+#include"threadtools.h"
 
 /*线程池类，将其定义为模板，T是任务类,任务类须定义process函数来执行具体任务*/
 template < typename T >
@@ -20,8 +20,9 @@ class threadpool
 {
 public:
     /* 参数1是线程池中线程的数量，参数2是请求队列中最多允许的、等待处理的请求的数量 */
-    threadpool(int thread_number = 8, int max_requests = 20000);
+    threadpool(int thread_number = 8, int max_requests = 10000);
     ~threadpool();
+
     /* 往请求队列中添加任务*/
     bool append(T* request);
 
@@ -34,14 +35,15 @@ private:
     int m_max_requests;         /* 请求队列最大请求数 */
     pthread_t* m_threads;       /* 描述线程池的数组，其大小为m_thread_number */
     std::list< T* > m_workqueue;/* 请求队列 */
-    locker m_queuelocker;       /* 请求队列锁 */ 
-    sem m_queuestat;            /* 是否有线程需要处理指示信号量 */
+    Mutex m_queuelocker;        /* 请求队列锁 */ 
+    Condition m_queuestat;      /* 是否有线程需要处理指示条件变量 */
     bool m_stop;                /* 是否结束线程 */
 };
 
 template < typename T >
 threadpool < T >::threadpool( int thread_number , int max_requests ) :
     m_thread_number( thread_number ),m_max_requests(max_requests),m_threads( NULL ),m_stop( false )
+    ,m_queuelocker(),m_queuestat(m_queuelocker)
 {
     if(( thread_number <= 0 ) || max_requests <= 0 )
     {
@@ -79,15 +81,16 @@ threadpool< T >::~threadpool()
 template < typename T >
 bool threadpool< T >::append(T * request)
 {
-    m_queuelocker.lock();
     if( m_workqueue.size() > m_max_requests )
     {
-        m_queuelocker.unlock();
         return false;
     }
-    m_workqueue.push_back( request );
-    m_queuelocker.unlock();
-    m_queuestat.post();
+
+    {
+        MutexGuard Lock(m_queuelocker);
+        m_workqueue.push_back( request );
+        m_queuestat.notify();
+    }
     return true;
 }
 
@@ -104,24 +107,24 @@ void threadpool< T >::run()
 {
     while( !m_stop )
     {
-        m_queuestat.wait();
-        m_queuelocker.lock();
-        if( m_workqueue.empty() )
+
+        T* request = NULL;
         {
-            m_queuelocker.unlock();
-            continue;
+            MutexGuard Lock(m_queuelocker);
+            while( m_workqueue.empty() )
+            {
+                m_queuestat.wait();
+            }
+            request = m_workqueue.front();
+            m_workqueue.pop_front();
+            
         }
-        T* request = m_workqueue.front();
-        m_workqueue.pop_front();
-        m_queuelocker.unlock();
+
         if( !request )
         {
             continue;
         }
-        if (request->run()) {
-            perror("run failed");
-            return;
-        }
+        request->start();
     }
 }
 
---
+++ src/threadtools.h
@@ -0,0 +1,95 @@
+#ifndef _LOCKER_H
+#define _LOCKER_H
+#include <pthread.h>
+#include <semaphore.h>
+#include <boost/utility.hpp>
+
+class Mutex : boost::noncopyable{
+    friend class Condition;
+public:
+    Mutex() {
+        pthread_mutex_init(&m_mutex,NULL);
+    }
+
+    ~Mutex() {
+        pthread_mutex_destroy(&m_mutex);
+    }
+
+    bool lock(){
+        return pthread_mutex_lock(&m_mutex) == 0;
+    }
+
+    bool unlock() {
+        return pthread_mutex_unlock(&m_mutex) == 0;
+    }
+
+private:
+    pthread_mutex_t m_mutex;
+};
+
+class MutexGuard : boost::noncopyable{
+public:
+    explicit MutexGuard(Mutex & mutex):m_mutex(mutex) {
+        m_mutex.lock();
+    }
+
+    ~MutexGuard() {
+        m_mutex.unlock();
+    }
+
+private:
+    Mutex& m_mutex;
+};
+
+
+class Condition : boost::noncopyable{
+public:
+    Condition(Mutex& mutex) : m_mutex(mutex) {
+        pthread_cond_init(&m_cond,NULL);
+    }
+
+    ~Condition() {
+        pthread_cond_destroy(&m_cond);
+    }
+
+    bool wait() {
+        return  pthread_cond_wait(&m_cond,&m_mutex.m_mutex) == 0;
+    }
+
+    bool notify() {
+        return pthread_cond_signal(&m_cond) == 0;
+    }
+
+    bool notifyAll() {
+        return pthread_cond_broadcast(&m_cond) == 0;
+    }
+
+private:
+    pthread_cond_t m_cond;
+    Mutex& m_mutex;
+};
+
+class Sem : boost::noncopyable{
+public:
+    Sem() {
+        sem_init(&m_sem,0,0);
+    }
+
+    ~Sem() {
+        sem_destroy(&m_sem);
+    }
+
+    bool wait() {
+        return sem_wait(&m_sem) == 0;
+    }
+
+    bool post() {
+        return sem_post(&m_sem) == 0;
+    }
+
+private:
+    sem_t m_sem;
+};
+
+#endif
+
